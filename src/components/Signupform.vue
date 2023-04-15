8e2d59c2643d545e743016edb17a85cd44aacd95
unknown <hasib8nh@gmail.com>
2023-04-15 16:28:10 +0600

    form submit

--- src/components/Signupform.vue
+++ src/components/Signupform.vue
@@ -4,12 +4,13 @@
 <!-- eslint-disable no-unused-vars -->
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <form>
+  <form @submit.prevent = "handleSubmit">
     <label> Email:</label>
     <input type="email" required v-model="email">
 
     <label> Password:</label>
     <input type="password" required v-model="password">
+    
     <label>Role:</label>
     <select v-model="role">
         <option value="developer"> Web Developer</option>
@@ -24,6 +25,9 @@
     <div class="terms">
         <input type="checkbox" v-model="terms" required>
         <label> Accept terms and condition</label>
+    </div>
+    <div class="submit">
+        <button> Create an Account</button>
     </div>
     <div>
         <input type="checkbox" v-model="names" value="Hasib">
@@ -71,6 +75,9 @@ export default {
             this.skills = this.skills.filter((item) => {
                 return skill !== item
             })
+        },
+        handleSubmit(){
+            console.log("form submitted");
         }
     }
 }
@@ -123,4 +130,16 @@ export default {
         color: #777;
         cursor: pointer;
     }
+    button {
+        background: #0b6dff;
+        border: 0;
+        padding: 10px 20px;
+        margin-top: 20px;
+        color: white;
+        border-radius: 20px;
+    }
+    .submit{
+        text-align: center;
+    }
+
 </style>
